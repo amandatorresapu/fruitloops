3b1c83ed9dd412f7720a1d680e7f882dab9107e5
Amanda Torres <69279617+amandatorresapu@users.noreply.github.com>
2022-04-09 17:23:30 -0700

    Add files via upload

---
+++ fruitLoops/src/main/java/com/codingdojo/fruitloops/controllers/HomeController.java
@@ -0,0 +1,34 @@
+package com.codingdojo.fruitloops.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codingdojo.fruitloops.models.Item;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		
+		model.addAttribute("toJsp", "meow");
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/dashboard")
+	public String dashboard(Model model) {
+		
+		ArrayList<Item> fruits = new ArrayList<Item>();
+        fruits.add(new Item("Kiwi", 1.5));
+        fruits.add(new Item("Mango", 2.0));
+        fruits.add(new Item("Goji Berries", 4.0));
+        fruits.add(new Item("Guava", .75));
+        model.addAttribute("addFruit", fruits);
+		return "dashboard.jsp";
+	}
+	
+
+}
